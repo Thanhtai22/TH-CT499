a6d8b59b9312d69029944d4c10defc16eddea951
thanhtai22 <tai127589@gmail.com>
2023-09-21 08:49:32 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.get("/",(req,res)=>{
     res.json({ messsage: " well com...."});
 })
+
+app.use("/api/contacts",contactsRouter)
 
 module.exports = app;
